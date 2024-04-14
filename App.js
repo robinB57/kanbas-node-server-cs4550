35350a327b671801d77c00c37852af76e3babf3f
robinB57 <bele.a@northeastern.edu>
2024-04-14 17:13:59 -0400

    Add allowed origins

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Kanbas routes
