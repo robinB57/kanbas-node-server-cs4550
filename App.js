c6bb44aa7908ef5bd90bd2a7344344dceb1e60d5
robin <bele.a@northeastern.edu>
2024-04-20 21:51:56 -0400

    Fix import

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import UserRoutes from "./Kanbas/Users/routes.js";
+import UserRoutes from "./Kanbas/users/routes.js";
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
